e0d56530a20b4ee75f009b516b0c765d629aae03
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-14 16:18:29 +0900

    #1 ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+     # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+     # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
   # enum role: { general: 1, admin: 99 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   devise_for :users
   root 'posts#index'
   resources :posts do
