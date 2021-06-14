673a6bf3714ad65b36797717857c5dab88ca4031
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-14 16:29:01 +0900

    #1 ゲストアカウントの編集・削除不可設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations',passwords: 'users/passwords'}
   root 'posts#index'
   resources :posts do
     collection do
