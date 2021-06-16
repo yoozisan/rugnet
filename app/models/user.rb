94179182008483e8f467203b1b1ac795cd36a951
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-16 20:55:23 +0900

    #5 コメント機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :image, ImageUploader
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :comments
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
