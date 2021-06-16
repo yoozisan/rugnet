0acf813c1c4b6950e70db910438826945b74e629
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-16 17:58:31 +0900

    #5 コメント機能アソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
   belongs_to :user
   validates :title, presence: true
+  validates :content, presence: true
   mount_uploader :image, ImageUploader
 end
