b8940ba72f61532a4273888bf969f85b0047636d
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-16 22:04:52 +0900

    マージ確認

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-
   # devise_scope :user do
   #   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   # end
