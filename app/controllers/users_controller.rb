bb27d6f1b3daa3df9bb725cd8380a714f28d1273
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-16 15:38:45 +0900

    #2 フォロー機能詳細に飛べるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
      @users = @user.posts.all
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_following'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index, :show]
   resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
   # devise_scope :user do
   #   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   # end
