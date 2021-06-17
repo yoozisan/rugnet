2745ec81b32a6aa7006ee55fbb011d1a58a77b16
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-17 22:14:10 +0900

    #10 studentとrecordのアソシエーション記述、recordのバリデーション記述

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,6 @@
 class Record < ApplicationRecord
   belongs_to :student
+  validates :record_at, presence: true
+  validates :body_temperature, presence: true
+  validates :attendance, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :user
+  has_many :records, dependent: :destroy
   validates :student_name, presence: true
   enum category: { A: 0, B: 1, C: 2, D: 3}
   enum school_year: {幼児: 0, "1年生": 1, "2年生": 2, "3年生": 3, "4年生": 4, "5年生": 5, "6年生": 6}
