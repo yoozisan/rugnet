8327877d1eef759a54a15408f6ce5921b43983c3
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-18 23:46:23 +0900

    #10 record新規登録画面修正中

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,5 +4,11 @@ class Record < ApplicationRecord
   validates :body_temperature, presence: true
   validates :attendance, presence: true
 
+
+  enum is_cough: { ❌:false, ⭕️:true }
+  enum is_sneeze: { ❌:false, ⭕️:true }
+  enum is_runny_nose: { ❌:false, ⭕️:true }
   enum is_dull: { ❌:false, ⭕️:true }
+  enum is_family_illness: { ❌:false, ⭕️:true }
+
 end
