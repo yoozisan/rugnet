4d10d5a9b2631d47cc214fd00ea2b164e239be53
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-24 17:47:23 +0900

    herokuデプロイのためコミット

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -3,6 +3,14 @@ module RecordsHelper
     if action_name == 'new' || action_name == 'create'
       confirm_student_records_path
     elsif action_name == 'edit'
+      confirm_student_record_path
+    end
+  end
+
+  def choose_new_or_edit_record_confirm
+    if action_name == 'new' || action_name == 'create'
+      student_records_path
+    elsif action_name == 'edit'
       student_record_path
     end
   end
