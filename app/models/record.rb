b0d9db1019bdd0a56bf80a5ffb4896cd95021192
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-25 00:48:01 +0900

    カテゴリーの説明追加、健康管理情報登録時のバリデーション追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,6 +2,17 @@ class Record < ApplicationRecord
   belongs_to :student
   validates :record_at, presence: true
   validates :body_temperature, presence: true
+  validate :record_date_cannot_be_in_the_future
+  def record_date_cannot_be_in_the_future
+    if record_at.present? && record_at > Date.today
+      errors.add(:record_at, "は未来を入力できません。")
+    end
+  end
+  validate :body_temperature_validate
+  def body_temperature_validate
+		errors.add(:body_temperature, "は35.0℃より高くなければなりません。") unless body_temperature.nil? || body_temperature > 35.0
+    errors.add(:body_temperature, "は42.0℃より低くなければなりません。") unless body_temperature.nil? || body_temperature < 42.0
+	end
 
   enum is_cough: { なし:false, あり:true }
   enum is_sneeze: { しない:false, する:true }
