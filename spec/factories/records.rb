ce16e248596dd141c18b9088c5fb42d188ce879e
yoichiro kawai <acchonburike.416@gmail.com>
2021-06-26 11:30:38 +0900

    systemテスト記述(record_spec)

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -7,16 +7,43 @@ FactoryBot.define do
     is_runny_nose { "出る"}
     is_dull { "だるくない"}
     is_family_illness { "いない"}
-    attendance { "出席"}
+    attendance { "欠席"}
   end
-  factory :record2,class: Record do
-    record_at { '2021-06-10' }
+  factory :record2, class: Record do
+    record_at { '2021-06-25' }
     body_temperature { 37.0}
-    association :student, factory: :student2
+    is_cough { "あり"}
+    is_sneeze { "する"}
+    is_runny_nose { "出る"}
+    is_dull { "だるい"}
+    is_family_illness { "いない"}
+    attendance { "欠席"}
+    # association :user, factory: :user
+    association :student, factory: :student
+
   end
-  factory :record3,class: Record do
-    record_at { '2021-04-21' }
-    body_temperature { 36.1}
-    association :student, factory: :student3
+  factory :record3, class: Record do
+    record_at { '2021-06-23' }
+    body_temperature { 36.2}
+    is_cough { "なし"}
+    is_sneeze { "する"}
+    is_runny_nose { "出る"}
+    is_dull { "だるくない"}
+    is_family_illness { "いる"}
+    attendance { "出席"}
+    # association :user, factory: :user
+    association :student, factory: :student
+
   end
+
+  # factory :record2,class: Record do
+  #   record_at { '2021-06-10' }
+  #   body_temperature { 37.0}
+  #   association :student, factory: :student2
+  # end
+  # factory :record3,class: Record do
+  #   record_at { '2021-04-21' }
+  #   body_temperature { 36.1}
+  #   association :student, factory: :student3
+  # end
 end
